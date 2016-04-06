456196ac7e2a4f19e898b9592c9b5c563f11fb4a
Dan Winkler <dan@6river.com>
2016-04-06 00:25:39 -0400

    Basic open/close unit tests with pseudo terminal

--- brain/brain_stem/src/brain_stem/test/SerialIO_test.cpp
+++ brain/brain_stem/src/brain_stem/test/SerialIO_test.cpp
@@ -6,26 +6,133 @@
  */
 
 #include "SerialIO.h"
-
 #include "gtest/gtest.h"
+#include <cstdlib>
+
+using namespace srs;
 
 namespace {
 
-class SerialIOTest : public ::testing::Test {};
-
-TEST( SerialIOTest, Open )
+class SerialIOTest : public ::testing::Test
 {
-	EXPECT_EQ(0, 0);
+private:
+
+public:
+	SerialIOTest( )
+	{
+
+	}
+
+	void SetUp( )
+	{
+
+	}
+
+	void TearDown( )
+	{
+
+	}
+
+	void ReadCallback( std::vector<char> buffer )
+	{
+
+	}
+
+	~SerialIOTest( )
+	{
+
+	}
+
+   // put in any custom data members that you need
+};
+
+TEST_F( SerialIOTest, OpenInvalidSerialPort )
+{
+    try
+    {
+    	SerialIO serial;
+    	serial.Open( "/foobar", std::bind( &SerialIOTest::ReadCallback, this,
+    		std::placeholders::_1 ) );
+
+    	FAIL( ) << "Expected std::exception";
+    }
+    catch( boost::system::system_error const & err )
+    {
+    	const char* pszError = err.what( );
+
+    	ASSERT_STREQ( "open: No such file or directory", pszError );
+    }
+    catch( ... )
+    {
+        FAIL( ) << "Expected std::exception";
+    }
 }
 
-TEST( SerialIOTest, Close )
+TEST_F( SerialIOTest, OpenSuccess )
 {
-	EXPECT_EQ(0, 0);
+    try
+    {
+    	SerialIO serial;
+    	serial.Open( "/tmp/pty1", std::bind( &SerialIOTest::ReadCallback, this,
+    		std::placeholders::_1 ) );
+
+    	EXPECT_TRUE( serial.IsOpen( ) );
+    }
+    catch( const std::exception& e )
+    {
+        FAIL( ) << "Open failed: " <<  e.what( );
+    }
+    catch( ... )
+    {
+        FAIL( ) << "Open failed: unknown exception";
+    }
 }
+
+TEST_F( SerialIOTest, Close )
+{
+    try
+    {
+    	SerialIO serial;
+    	serial.Open( "/tmp/pty1", std::bind( &SerialIOTest::ReadCallback, this,
+    		std::placeholders::_1 ) );
+
+    	EXPECT_TRUE( serial.IsOpen( ) );
+
+    	serial.Close( );
+
+    	EXPECT_FALSE( serial.IsOpen( ) );
+    }
+    catch( const std::exception& e )
+    {
+        FAIL( ) << "Open failed: " <<  e.what( );
+    }
+    catch( ... )
+    {
+        FAIL( ) << "Open failed: unknown exception";
+    }}
 
 }  // namespace
 
 int main(int argc, char **argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+
+	// Open up two pseudo serial ports that communicate with each other
+	// /tmp/pty1 and /tmp/pty2
+	std::string strCommand;
+	strCommand += "touch /tmp/pty1 &&";
+	strCommand += "touch /tmp/pty2 &&";
+	strCommand += "socat -d -d pty,link=/tmp/pty1,raw,echo=0 pty,link=/tmp/pty2,raw,echo=0";
+	strCommand += " &"; // Don't wait (background)
+
+	std::system( strCommand.c_str( ) );
+
+	sleep( 1 );
+
+	::testing::InitGoogleTest(&argc, argv);
+
+	int success = RUN_ALL_TESTS( );
+
+	std::system( "killall socat" );
+
+	return success;
+
 }

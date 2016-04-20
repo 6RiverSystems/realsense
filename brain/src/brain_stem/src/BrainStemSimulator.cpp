304b21804240313136afd34421772088000efcda
Dan Winkler <dan@6river.com>
2016-04-20 11:49:46 -0400

    Cleaned up simulated pose debug messages

--- brain/src/brain_stem/src/BrainStemSimulator.cpp
+++ brain/src/brain_stem/src/BrainStemSimulator.cpp
@@ -30,7 +30,7 @@ void RawOdometryVelocity( const geometry_msgs::TwistStamped& estimatedVelocity )
 
 	g_velocity = estimatedVelocity.twist;
 
-	ROS_DEBUG_THROTTLE( 5.0f, "Estimated Velocity Changed: linear=%f, angular=%f",
+	ROS_DEBUG( "Estimated Velocity Changed: linear=%f, angular=%f",
 		g_velocity.linear.x, g_velocity.angular.z );
 }
 
@@ -83,7 +83,7 @@ int main(int argc, char** argv) {
 
 		if( dfXDelta || dfYDelta || dfThetaDelta )
 		{
-			ROS_DEBUG_THROTTLE( 5.0f, "Pose Changed: x=%lf, y=%lf, theta=%lf",
+			ROS_DEBUG_THROTTLE( 1.0f, "Pose Changed: x=%lf, y=%lf, theta=%lf",
 				dfX, dfY, dfTheta );
 		}
 

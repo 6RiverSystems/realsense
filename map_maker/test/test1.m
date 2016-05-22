8637c1a03d3e0f87739111fa5ad643b88dba776f
Fabrizio Santini <fsantini@6river.com>
2016-05-22 07:02:12 -0400

    Fixed few bugs in the Map Maker.

--- map_maker/test/test1.m
+++ map_maker/test/test1.m
@@ -1,7 +1,7 @@
 map = mm_create(10, 18, 0.1);
 
 map = mm_addBorder(map, 0.5);
-map = addStaticObstacle(map, 0, 0, 0, 0, 0.5, 255);
+map = mm_addStaticObstacle(map, 0, 0, 0, 0, 0.5, 255);
 
 map = mm_generate(map);
 mm_save(map, '.', 'test');

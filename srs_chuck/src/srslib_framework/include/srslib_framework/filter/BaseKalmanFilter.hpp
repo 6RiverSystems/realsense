67ad21aa8a589bc71bfcf530c090ecfc4de19011
Fabrizio Santini <fsantini@6river.com>
2016-06-01 10:55:15 -0400

    Temporarily removed the debug message of the KF covariance.

--- srs_chuck/src/srslib_framework/include/srslib_framework/filter/BaseKalmanFilter.hpp
+++ srs_chuck/src/srslib_framework/include/srslib_framework/filter/BaseKalmanFilter.hpp
@@ -66,7 +66,7 @@ public:
 
         ROS_INFO_STREAM_NAMED("BaseKalmanFilter", "Current state: ");
         ROS_INFO_STREAM_NAMED("BaseKalmanFilter", test::Print::printToString(x_, "x_"));
-        ROS_INFO_STREAM_NAMED("BaseKalmanFilter", test::Print::printToString(P_, "P_"));
+        // ROS_INFO_STREAM_NAMED("BaseKalmanFilter", test::Print::printToString(P_, "P_"));
     }
 
 protected:

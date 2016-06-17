26990912b366714a67db2be496a8eac3acf359f0
Dan Winkler <dan@6river.com>
2016-06-17 14:28:01 -0400

    Added stargazer filter.

--- srs_chuck/src/srsdrv_stargazer/src/StarGazerPointTransformer.cpp
+++ srs_chuck/src/srsdrv_stargazer/src/StarGazerPointTransformer.cpp
@@ -129,7 +129,7 @@ bool StarGazerPointTransformer::TransformPoint( int nTagId, double fX, double fY
 
 	if( iter != m_mapTransforms.end( ) )
 	{
-		if( true )//m_filter.unfilterStargazerData( nTagId, fX, fY, fZ ) )
+		if( m_filter.unfilterStargazerData( nTagId, fX, fY, fZ ) )
 		{
 			// The anchor transform from the origin of the map to the anchor point
 			const tf::Transform& anchorGlobal = iter->second;
@@ -223,7 +223,7 @@ bool StarGazerPointTransformer::TransformPoint( int nTagId, double fX, double fY
 
 				std::string strData =  stream.str( );
 
-				ROS_ERROR_NAMED( "StarGazerPointTransformer", "%s", strData.c_str( ) );
+				ROS_INFO_NAMED( "StarGazerPointTransformer", "%s", strData.c_str( ) );
 
 				// reset accumulators
 				accLocalX = double_acc( );

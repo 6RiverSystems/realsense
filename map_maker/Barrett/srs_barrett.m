e27a36bc0cc2e6b90e9ba69f4d65d7118599d975
Fabrizio Santini <fsantini@6river.com>
2016-07-01 12:28:37 -0400

    Updated Barrett map with bigger buffer zones.

--- map_maker/Barrett/srs_barrett.m
+++ map_maker/Barrett/srs_barrett.m
@@ -10,16 +10,16 @@ map = mm_create(35, 28, 0.10);
 map = mm_addCustomBorder(map, 11.201, 1.50, 29.420, 27.137, 0.4);
 
 % Obstacle RE02
-map = mm_addStaticObstacle(map, 14.113, 5.007, 16.485, 23.759, 0.4);
+map = mm_addStaticObstacle(map, 14.113, 5.007, 16.485, 23.759, 0.8);
 
 % Obstacle RE03
-map = mm_addStaticObstacle(map, 18.307, 5.007, 20.674, 23.759, 0.5);
+map = mm_addStaticObstacle(map, 18.307, 5.007, 20.674, 23.759, 0.8);
 
 % Obstacle RE04
-map = mm_addStaticObstacle(map, 22.491, 5.007, 25.048, 23.759, 0.5);
+map = mm_addStaticObstacle(map, 22.491, 5.007, 25.048, 23.759, 0.8);
 
 % Obstacle RE05
-map = mm_addStaticObstacle(map, 26.869, 5.007, 29.420, 23.759, 0.5);
+map = mm_addStaticObstacle(map, 26.869, 5.007, 29.420, 23.759, 0.8);
 
 % Obstacle OB01
 map = mm_addStaticObstacle(map, 23.253, 2.981, 24.006, 3.235, 0.5);

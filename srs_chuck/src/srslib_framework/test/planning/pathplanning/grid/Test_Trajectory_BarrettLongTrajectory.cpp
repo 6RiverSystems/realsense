44d6ad097745397bc6319948576946d34bfbad69
Fabrizio Santini <fsantini@6river.com>
2016-07-19 17:15:58 -0400

    Corrected name of the test and a compilation error.

--- srs_chuck/src/srslib_framework/test/planning/pathplanning/grid/Test_Trajectory_BarrettLongTrajectory.cpp
+++ srs_chuck/src/srslib_framework/test/planning/pathplanning/grid/Test_Trajectory_BarrettLongTrajectory.cpp
@@ -24,7 +24,7 @@ using namespace std;
 
 #include <srslib_framework/search/AStar.hpp>
 
-#include <srslib_test/planning/localization/MapUtils.hpp>
+#include <srslib_test/localization/MapUtils.hpp>
 #include <srslib_test/planning/pathplanning/PathPlanningUtils.hpp>
 
 using namespace srs;
@@ -40,5 +40,5 @@ TEST(Test_Trajectory, BarrettLongTrajectory)
 
     Pose<> robotPose = Pose<>(14.7855, 4.65954, 1.53943);
     Pose<> goalPose = Pose<>(33.215, 51.703, 1.5708);
-    Solution<GridSolutionItem>* solution = PathPlanningUtils::pose2Solution(map, robotPose, goalPose);
+    Solution<GridSolutionItem>* solution = test::PathPlanningUtils::pose2Solution(map, robotPose, goalPose);
 }

d3a57a62a837f6979b9e4d02c0b0631546dd96f7
Fabrizio Santini <fsantini@6river.com>
2016-08-09 10:27:39 -0400

    Two additional stalls in the sort wall map.

--- map_maker/6RS-HQ/srs_hq_sortwall.m
+++ map_maker/6RS-HQ/srs_hq_sortwall.m
@@ -17,14 +17,20 @@ map = mm_addStaticObstacle(map, 6.253, 3.168, 16.692, 4.387, 0.7);
 map = mm_addStaticObstacle(map, 3.662, 6.216, 16.692, 7.435, 0.7);
 
 % Obstacle Workshop
-map = mm_addStaticObstacle(map, 9.148, 9.264, 19.521, 12.264, 0.7);
+map = mm_addStaticObstacle(map, 9.148, 10.264, 19.521, 12.264, 0.7);
 
 % Obstacle Stall 1
-map = mm_addStaticObstacle(map, 3.662, 8.664, 3.762, 9.664, 0.3);
+map = mm_addStaticObstacle(map, 3.362, 8.664, 3.462, 9.664, 0.3);
 
 % Obstacle Stall 2
-map = mm_addStaticObstacle(map, 4.912, 8.664, 5.012, 9.664, 0.3);
+map = mm_addStaticObstacle(map, 4.512, 8.664, 4.612, 9.664, 0.3);
+
+% Obstacle Stall 3
+map = mm_addStaticObstacle(map, 5.762, 8.664, 5.862, 9.664, 0.3);
+
+% Obstacle Stall 4
+map = mm_addStaticObstacle(map, 7.012, 8.664, 7.112, 9.664, 0.3);
 
 map = mm_generate(map);
-mm_save(map, '../../srs_sites/src/srsc_6rhq/map', '6rhq_sortwall');
-mm_save(map, '../../srs_sites/src/srsc_6rhq_rviz/map', '6rhq_sortwall');
+mm_save(map, '../../srs_sites/src/srsc_6rhq_sortwall/map', '6rhq_sortwall');
+mm_save(map, '../../srs_sites/src/srsc_6rhq_sortwall_rviz/map', '6rhq_sortwall');

7ec9987c25ceeb04d4d85b1fae6958f339b99512
Dan Winkler <dan@6river.com>
2016-09-16 15:42:06 -0400

    Fixed odometry timestamp

--- srs_chuck/src/srsnode_odometry/src/OdometryPositionEstimator.cpp
+++ srs_chuck/src/srsnode_odometry/src/OdometryPositionEstimator.cpp
@@ -109,9 +109,8 @@ void OdometryPositionEstimator::RawOdometryVelocity( const geometry_msgs::TwistS
 
 	geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw( pose_.theta );
 
-	ros::Time current = ros::Time::now( );
 	// Publish the TF
-	odom_trans.header.stamp = current;
+	odom_trans.header.stamp = currentTime;
 	odom_trans.transform.translation.x = pose_.x;
 	odom_trans.transform.translation.y = pose_.y;
 	odom_trans.transform.translation.z = 0.0;
@@ -120,7 +119,7 @@ void OdometryPositionEstimator::RawOdometryVelocity( const geometry_msgs::TwistS
 	broadcaster_.sendTransform( odom_trans );
 
 	nav_msgs::Odometry odom;
-	odom.header.stamp = current;
+	odom.header.stamp = currentTime;
 	odom.header.frame_id = "odom";
 	odom.child_frame_id = "base_footprint";
 

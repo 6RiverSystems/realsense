611979dde1eab39bc7f88fb3f28ad09da00c4fa5
Fabrizio Santini <fsantini@6river.com>
2016-10-18 16:50:00 -0400

    Measured performance with the new A* implementation. The algorithm is 2.7 faster than the previous one (without additional memory management).

--- srs_chuck/src/srslib_framework/test/search/Test_AStar_Barrett_BigSearch.cpp
+++ srs_chuck/src/srslib_framework/test/search/Test_AStar_Barrett_BigSearch.cpp
@@ -10,25 +10,49 @@ using namespace std;
 
 #include <ros/ros.h>
 
+#include <srslib_framework/datastructure/graph/grid2d/Grid2d.hpp>
 #include <srslib_framework/localization/map/MapStack.hpp>
 #include <srslib_framework/localization/map/MapStackFactory.hpp>
-#include <srslib_framework/planning/pathplanning/grid2d/Grid2dSolutionFactory.hpp>
-#include <srslib_framework/planning/pathplanning/grid2d/Grid2dSolutionItem.hpp>
+#include <srslib_framework/search/AStar.hpp>
+#include <srslib_framework/search/graph/grid2d/Grid2dPosition.hpp>
+#include <srslib_framework/search/graph/grid2d/Grid2dNode.hpp>
+#include <srslib_framework/search/graph/grid2d/Grid2dSingleGoal.hpp>
 
+#include <srslib_test/utils/MemoryWatch.hpp>
 using namespace srs;
 
 TEST(Test_AStar, Barrett_BigSearch)
 {
-    // ###FS
-//    MapStack* mapStack = MapStackFactory::fromJsonFile("pathplanning/grid2d/data/barrett/barrett.yaml");
-//
-//    // Looking for a path between
-//    // Pose {@: 1.4686e+09, x: 14.7855, y: 4.65954, t: 1.53943} (148,47,90)
-//    // and
-//    // Pose {@: 1.4686e+09, x: 33.215, y: 51.703, t: 1.5708} (332,522,90)
-//
-//    Pose<> robotPose = Pose<>(14.7855, 4.65954, 1.53943);
-//    Pose<> goalPose = Pose<>(33.215, 51.703, 1.5708);
-//    Solution<Grid2dSolutionItem>* solution = Grid2dSolutionFactory::fromGoal(
-//        mapStack->getOccupancyMap(), robotPose, goalPose);
+    MapStack* mapStack = MapStackFactory::fromJsonFile("data/barrett/barrett.yaml");
+
+    // Looking for a path between
+    // Pose {@: 1.4686e+09, x: 14.7855, y: 4.65954, t: 1.53943} (148, 47, 90)
+    // and
+    // Pose {@: 1.4686e+09, x: 33.215, y: 51.703, t: 1.5708} (332, 522, 90)
+    Grid2dPosition startPosition(Grid2d::Location(148, 47), 90);
+    Grid2dPosition goalPosition(Grid2d::Location(332, 522), 90);
+
+    test::MemoryWatch memoryWatch;
+
+    AStar* algorithm = new AStar();
+    Grid2dNode* start = Grid2dNode::instanceOfStart(mapStack->getLogicalMap()->getGrid(),
+        startPosition);
+    Grid2dSingleGoal* goal = Grid2dSingleGoal::instanceOf(goalPosition);
+
+    ASSERT_TRUE(algorithm->search(start, goal)) <<
+        "A solution was not found";
+
+    ASSERT_EQ(31189, algorithm->getOpenNodeCount()) <<
+        "Unexpected number of open nodes";
+
+    ASSERT_EQ(27734, algorithm->getClosedNodeCount()) <<
+        "Unexpected number of closed nodes";
+
+    algorithm->clear();
+
+    start->release();
+    goal->release();
+    delete algorithm;
+
+    ASSERT_TRUE(memoryWatch.isZero()) << "Memory leaks occurred";
 }

7a2e7a85913a9d70304688bf65aea34c73eae0bf
Fabrizio Santini <fsantini@6river.com>
2016-12-15 13:48:25 -0500

    Fixed a small bug in the equal operation of the Logical Map that was making a test to fail.

--- srs_chuck/src/srslib_framework/src/localization/map/logical/LogicalMap.cpp
+++ srs_chuck/src/srslib_framework/src/localization/map/logical/LogicalMap.cpp
@@ -110,9 +110,39 @@ ostream& operator<<(ostream& stream, const LogicalMap& map)
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 bool operator==(const LogicalMap& lhs, const LogicalMap& rhs)
 {
-    return lhs.metadata_ == rhs.metadata_ &&
-        operator==(static_cast<const BaseMap&>(lhs), static_cast<const BaseMap&>(rhs)) &&
-        lhs.labeledAreas_ == rhs.labeledAreas_;
+    if (&lhs == &rhs)
+    {
+        return true;
+    }
+
+    if (lhs.metadata_ == rhs.metadata_ &&
+        operator==(static_cast<const BaseMap&>(lhs), static_cast<const BaseMap&>(rhs)))
+    {
+        if (lhs.labeledAreas_.size() != rhs.labeledAreas_.size())
+        {
+            return false;
+        }
+
+        for (auto area : lhs.labeledAreas_)
+        {
+            if (rhs.labeledAreas_.find(area.first) == rhs.labeledAreas_.end())
+            {
+                return false;
+            }
+        }
+
+        for (auto area : rhs.labeledAreas_)
+        {
+            if (lhs.labeledAreas_.find(area.first) == lhs.labeledAreas_.end())
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    return false;
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////

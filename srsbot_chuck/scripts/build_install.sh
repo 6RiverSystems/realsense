df47440c63b5ac0f3a9246a8bce8d4d7a09a54fd
Dan Winkler <dan@6river.com>
2017-04-10 08:54:10 -0400

    Attempt using docker and 1.0

--- srsbot_chuck/scripts/build_install.sh
+++ srsbot_chuck/scripts/build_install.sh
@@ -3,12 +3,13 @@ export PATH=/usr/lib/ccache:$PATH
 export ROS_PARALLEL_JOBS='-j8 -l8'
 export ROS_LANG_DISABLE=genlisp:roslisp
 
+cd /mfp_workspace/src
+catkin_init_workspace
+
 cd /mfp_workspace
 
-wstool init src
-
 # compile and run unit tests
-time catkin_make -j4 run_tests
+time catkin_make run_tests
 
 # generate unified test results
-time catkin_test_results -j4 build/test_results
+time catkin_test_results build/test_results
--- srsbot_chuck/scripts/run_unit_tests.sh
+++ srsbot_chuck/scripts/run_unit_tests.sh
@@ -3,10 +3,13 @@ export PATH=/usr/lib/ccache:$PATH
 export ROS_PARALLEL_JOBS='-j8 -l8'
 export ROS_LANG_DISABLE=genlisp:roslisp
 
+cd /mfp_workspace/src
+catkin_init_workspace
+
 cd /mfp_workspace
 
 # compile and run unit tests
-time catkin_make -j4 install
+time catkin_make install
 
 # generate unified test results
-time catkin_test_results -j4 build/test_results
+time catkin_test_results build/test_results

dc10f127676118d0d50e765dde63172ad34b31e9
Marc Rousseau <mrousseau@6river.com>
2017-05-08 14:41:44 -0400

    Fix missing case in switch statement for enums

--- srslib_framework/include/srslib_framework/planning/pathplanning/trajectory/SimpleTrajectoryGenerator.hpp
+++ srslib_framework/include/srslib_framework/planning/pathplanning/trajectory/SimpleTrajectoryGenerator.hpp
@@ -78,6 +78,8 @@ private:
                 case Grid2dSolutionItem::ROTATE:
                     rotations.push_back(solutionNode.toPose);
                     break;
+                case Grid2dSolutionItem::NONE:
+                    break;
             }
         }
 

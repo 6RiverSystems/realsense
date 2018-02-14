2ec86fa6b157f4db8d413a047ab2fd13d4dc5784
Konrad Malkowski <kmalkowski@6river.com>
2018-02-13 19:54:08 -0500

    fix: patch a bug

--- realsense_ros_camera/src/realsense_node_factory.cpp
+++ realsense_ros_camera/src/realsense_node_factory.cpp
@@ -69,6 +69,7 @@ void RealSenseNodeFactory::onInit()
             else if (port_id == usb_port_id)
             {
                 _device = dev;
+                serial_no = serial_number;
                 found = true;
                 ROS_INFO_STREAM("Device connected with USB port: " << port_id << " (serial number: " << serial_number << ") was found.");
                 break;
@@ -87,7 +88,7 @@ void RealSenseNodeFactory::onInit()
         ROS_INFO_STREAM("RESETING DEVICE: " << port_id << " with serial number: " << serial_no);
         _device.hardware_reset();
         ros::Duration(5).sleep();
-        ROS_INFO_STREAM("Attempting to reacquire");
+        ROS_INFO_STREAM("Attempting to reacquire device: " << port_id << " with serial number: " << serial_no);
 
         list = _ctx.query_devices();
         if (0 == list.size())
@@ -100,8 +101,7 @@ void RealSenseNodeFactory::onInit()
         for (auto&& dev : list)
         {
             auto sn = dev.get_info(RS2_CAMERA_INFO_SERIAL_NUMBER);
-            auto port_id = parseUsbPortId(dev.get_info(RS2_CAMERA_INFO_PHYSICAL_PORT));
-            if (sn == serial_no && port_id == usb_port_id)
+            if (sn == serial_no )
             {
                 _device = dev;
                 serial_no = sn;

4970278b1b797a2f31eb1a68d1bee999f8d442a3
zach <zachfang@6river.com>
2018-03-15 21:46:02 -0400

    fix: PR change

--- realsense_camera/include/realsense_camera/base_nodelet.h
+++ realsense_camera/include/realsense_camera/base_nodelet.h
@@ -132,7 +132,6 @@ protected:
   cv::Mat cvWrapper_;
   std::mutex frame_mutex_[STREAM_COUNT];
 
-  boost::shared_ptr<boost::thread> transform_thread_;
   ros::Time transform_ts_;
   tf2_ros::StaticTransformBroadcaster static_tf_broadcaster_;
   tf::TransformBroadcaster dynamic_tf_broadcaster_;
@@ -174,7 +173,6 @@ protected:
   virtual void getCameraExtrinsics();
   virtual void publishStaticTransforms();
   virtual void publishDynamicTransforms();
-  virtual void prepareTransforms();
   virtual void checkError();
   virtual bool checkForSubscriber();
   virtual void wrappedSystem(const std::vector<std::string>& string_argv);

0b5fb1a4b6b7aaae70e63530f5e796451b029ff3
Konrad Malkowski <kmalkowski@6river.com>
2018-06-12 13:43:17 -0400

    fix: do not reset the camera after acquiring

--- realsense2_camera/src/realsense_node_factory.cpp
+++ realsense2_camera/src/realsense_node_factory.cpp
@@ -84,39 +84,7 @@ void RealSenseNodeFactory::onInit()
             exit(1);
         }
 
-        // we found the device. Restart the device and wait for it to come up again
-
-        ROS_INFO_STREAM("RESETING DEVICE: " << port_id << " with serial number: " << serial_no);
-        _device.hardware_reset();
-        ros::Duration(5).sleep();
-        ROS_INFO_STREAM("Attempting to reacquire device: " << port_id << " with serial number: " << serial_no);
-
-        list = _ctx.query_devices();
-        if (0 == list.size())
-        {
-            ROS_ERROR("No RealSense devices were found! Terminating RealSense Node...");
-            ros::shutdown();
-            exit(1);
-        }
-        found = false;
-        for (auto&& dev : list)
-        {
-            auto sn = dev.get_info(RS2_CAMERA_INFO_SERIAL_NUMBER);
-            if (sn == serial_no )
-            {
-                _device = dev;
-                found = true;
-                break;
-            }
-        }
-
-        if (!found)
-        {
-            ROS_FATAL_STREAM("The requested device at USB port: " << usb_port_id << " is NOT found!");
-            ros::shutdown();
-            exit(1);
-        }
-
+        // we found the device.
         _ctx.set_devices_changed_callback([this](rs2::event_information& info)
         {
             if (info.was_removed(_device))

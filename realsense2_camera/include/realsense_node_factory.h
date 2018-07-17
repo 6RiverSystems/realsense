28d8ce74410d26c280507c344eee3c42389e4a7f
Dan Winkler <dan@6river.com>
2018-07-17 09:33:36 -0400

    fix: Support older non resin.io bots.

--- realsense2_camera/include/realsense_node_factory.h
+++ realsense2_camera/include/realsense_node_factory.h
@@ -66,6 +66,7 @@ namespace realsense2_camera
 
     private:
         virtual void onInit() override;
+        void onInitLegacy();
         void tryGetLogSeverity(rs2_log_severity& severity) const;
         std::string parseUsbPortId(std::string usb_path) const;
         bool deviceMatches(rs2::device& dev, std::string& usb_port);

27a7a15210d5508457e1204c9c867c8b9d9de3a3
Dan Winkler <dan@6river.com>
2018-10-26 09:37:54 -0400

    Fix: Remove forward timestamping.

--- realsense2_camera/src/base_realsense_node.cpp
+++ realsense2_camera/src/base_realsense_node.cpp
@@ -1356,8 +1356,7 @@ void BaseRealSenseNode::publishDynamicTransforms()
     // depth to ir and ir to ir_optical
 
     // add 2 times of publication interval in the future to avoid tf extrapolation error
-    float futureDate = 2.0 * (1.0 / _fps[DEPTH]);
-    ros::Time transform_ts = ros::Time::now() + ros::Duration(futureDate);
+    ros::Time transform_ts = ros::Time::now();
     tf::Transform tr;
     tf::Transform tr_optical;
 

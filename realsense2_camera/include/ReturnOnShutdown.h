26408de9097ef55d8fccd47224f8b1a1fb7f48ba
Konrad Malkowski <kmalkowski@6river.com>
2018-12-12 15:58:24 -0500

    fix: change timing and a debug statement

--- realsense2_camera/include/ReturnOnShutdown.h
+++ realsense2_camera/include/ReturnOnShutdown.h
@@ -14,12 +14,15 @@
 #define REQUEST_SHUTDOWN()                                           \
 {                                                                    \
     RETURN_ON_SHUTDOWN();                                            \
+    ROS_ERROR_STREAM("Requesting USB bus reset");                    \
     std_msgs::Bool msg;                                              \
     msg.data = true;                                                 \
     _reset_request_publisher.publish(msg);                           \
+    sleep(1);                                                        \
     ros::requestShutdown();                                          \
+    sleep(1);                                                        \
     ros::shutdown();                                                 \
-    sleep(5);                                                        \
+    sleep(3);                                                        \
     return;                                                          \
 }
 #endif
